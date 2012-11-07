7e2a06c5d7f81317db6784b8217e8a10cb8f236b
Philipp Gruhn <philipp.gruhn@googlemail.com>
2012-11-07 21:44:02 +0100

    Change-Id: I84b343dcfc15970feb5bac543cacca3b96cecf67

--- R_Interface/src/experimental/RConnection.java
+++ R_Interface/src/experimental/RConnection.java
@@ -6,10 +6,16 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RConnection
+import de.hsl.rinterface.Connection;
+import de.hsl.rinterface.commands.RCommand;
+import de.hsl.rinterface.exception.RException;
+import de.hsl.rinterface.objects.RObject;
+
+public class RConnection implements Connection
 {
 	private Process proc;
 	private final BufferedReader errRd, outRd;
@@ -30,49 +36,17 @@ public class RConnection
 				proc.getInputStream()));
 		inWr = new BufferedWriter(new OutputStreamWriter(new BufferedOutputStream(
 					proc.getOutputStream())));
-	}
-
-	/**
-	 * Schließt sowohl alle offenen Ressourcen, als auch den ausgeführten R-Prozess.
-	 * Kehrt zurück, wenn der Prozess beendet wurde.
-	 * @throws InterruptedException
-	 * @throws IOException falls der Prozess bereits beendet sein sollte
-	 */
-	public void closeConnection() throws InterruptedException, IOException
-	{
-		//Gebe Befehl zum Schlie�en der Anwendung
-		sendCommand("q()");
-		//Warten bis Prozess terminiert hat
-		proc.waitFor();
-		System.out.println(proc.exitValue());
-		
-		errRd.close();
-		outRd.close();
-		inWr.close();
-		//kill, falls noch nicht erfolgt
-		proc.destroy();
+		//Blocke bis zum Eintreffen der Willkommensnachricht
+		outRd.readLine();
+		while(outRd.ready())
+		{	//Verwerfen
+			outRd.readLine();
+		}	
 	}
 	
-	/**
-	 * Sendet den übergebenen Befehl sofort an die Konsole. Fügt einen Zeilenumbruch an. 
-	 * @param cmd der auszuführende Befehl
-	 * @throws IOException falls bei der Übermittlung zum Prozess ein Fehler auftritt
-	 */
-	public void sendCommand(String cmd) throws IOException
+	public void max(List<? extends Number> list) throws RException
 	{
-		inWr.write(cmd);
-		inWr.newLine();
-		inWr.flush();
-	}
-	
-	public void testSth() throws IOException
-	{
-		sendCommand("");
-	}
-	
-	public void max(List<? extends Number> list) throws IOException
-	{
-		sendCommand("max(" + resolveList(list) + ")");
+		sendCmd("max(" + resolveList(list) + ")");
 	}
 	
 	public String resolveList(List<? extends Number> list)
@@ -90,26 +64,99 @@ public class RConnection
 		return listCmd;
 	}
 	
-	public static void main(String[] args)
+
+	/**
+	 * Schließt sowohl alle offenen Ressourcen, als auch den ausgeführten R-Prozess.
+	 * Kehrt zurück, wenn der Prozess beendet wurde.
+	 * @throws InterruptedException
+	 * @throws RException 
+	 * @throws IOException falls der Prozess bereits beendet sein sollte
+	 */
+	@Override
+	public void close() throws InterruptedException, RException
 	{
-		int count = 0;
-		List<Integer> list = new ArrayList<>();
-			list.add(2);
-			list.add(354);
-			list.add(1);
-			list.add(5);
-			list.add(-5);
-		String listCmd = "c(";
-		
-		for(Number n : list)
+		//Gebe Befehl zum Schließen der Anwendung
+		sendCmd("q()");
+		//Warten bis Prozess terminiert hat
+		proc.waitFor();
+		System.out.println(proc.exitValue());
+				
+		try
 		{
-			if(++count != list.size())
-				listCmd += n.toString() + ", ";
-			else
-				listCmd += n.toString() + ")";
+			errRd.close();
+			outRd.close();
+			inWr.close();
+		} 
+		catch (IOException e)
+		{
+			throw new RException(e);
 		}
-	
-		System.out.println(listCmd);
+		//kill, falls noch nicht erfolgt
+		proc.destroy();
+	}
+
+	@Override
+	public boolean isAlive()
+	{
+		// TODO Auto-generated method stub
+		return outRd != null;
+	}
+
+	@Override
+	/**
+	 * Sendet den übergebenen Befehl sofort an die Konsole. Fügt einen Zeilenumbruch an. 
+	 * @param cmd der auszuführende Befehl
+	 * @throws IOException falls bei der Übermittlung zum Prozess ein Fehler auftritt
+	 */
+		
+	public RObject sendCmd(String cmd) throws RException
+	{
+		try
+		{
+			PrintWriter pw = new PrintWriter(inWr);
+			pw.println(cmd);
+			pw.flush();
+//			inWr.write(cmd);
+//			inWr.newLine();
+//			inWr.flush();
+			//Eingabe drin, auf Ausgabe horchen
+			System.out.println("gesendet");
+			while(outRd.ready())
+			{
+				System.out.println("lese");
+				String readLine = outRd.readLine();
+				System.out.println(readLine);
+				
+			}	
+		}
+		catch (IOException e) 
+		{e.printStackTrace();
+			throw new RException(e);
+			
+		}
+		
+		return null;
+	}
+
+	@Override
+	public RObject sendCmd(RCommand cmd)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getStatus()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getAllVars()
+	{
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
