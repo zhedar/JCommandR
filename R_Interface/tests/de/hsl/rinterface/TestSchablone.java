8e9b0cc3c64e352f5f2c9a4ea15d00bbb9558870
Pegk <pkuebe@hs-lausitz.de>
2012-12-07 16:56:40 +0100

    TestSchablone: Mean funktioniert nicht

--- R_Interface/tests/de/hsl/rinterface/TestSchablone.java
+++ R_Interface/tests/de/hsl/rinterface/TestSchablone.java
@@ -8,10 +8,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.hsl.rinterface.commands.RMean;
 import de.hsl.rinterface.exception.RException;
 import de.hsl.rinterface.objects.RMatrix;
 import de.hsl.rinterface.objects.RObject;
 import de.hsl.rinterface.objects.RObjectTypes;
+import de.hsl.rinterface.objects.RValue;
+import de.hsl.rinterface.objects.RVector;
 
 public class TestSchablone
 {
@@ -26,17 +29,34 @@ public class TestSchablone
 	
 	//Dies ist ein Beispiel
 	@Test
-	public void parse5_5Matrix() throws RException
+	public void mean() throws RException
 	{
-		RObject ro = con.sendCmd("matrix(1,5,5)");
-		if (ro.getType().equals(RObjectTypes.MATRIX))
-		{	//Ergebnis ist eine Matrix - prima
-			RMatrix name = (RMatrix) ro;
-			Assert.assertNotNull(name);
-			Assert.assertTrue(name.getRowLength()== 5 && name.getColLength() == 5);		
-		}
-		else	//Test fehlschlagen lassen, falls if nicht ausgelöst wurde
-			Assert.fail("Ergebnis ist keine Matrix, sollte aber eine sein.");
+//		RObject ro = con.sendCmd("matrix(1,5,5)");
+//		if (ro.getType().equals(RObjectTypes.MATRIX))
+//		{	//Ergebnis ist eine Matrix - prima
+//			RMatrix name = (RMatrix) ro;
+//			Assert.assertNotNull(name);
+//			Assert.assertTrue(name.getRowLength()== 5 && name.getColLength() == 5);		
+//		}
+//		else	//Test fehlschlagen lassen, falls if nicht ausgelöst wurde
+//			Assert.fail("Ergebnis ist keine Matrix, sollte aber eine sein.");
+		
+		RVector<Double> rv = new RVector<>();
+		rv.add(1.0);
+		rv.add(5.0);
+		rv.add(90.0);
+		rv.add(25.0);
+		
+		RObject ro = con.sendCmd(new RMean(rv));
+		
+		if (ro.getType().equals(RObjectTypes.VALUE)) {
+			RValue<Double> value = (RValue<Double>) ro;
+			Assert.assertNotNull(value);
+			Assert.assertTrue(value.getValue() == 30.25);		
+			}
+			else	//Test fehlschlagen lassen, falls if nicht ausgelöst wurde
+				Assert.fail("Ergebnis ist keine Value, sollte aber eine sein."+rv.getType()+ro);
+		
 	}
 	
 	@After
