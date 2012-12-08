471b377723ab5d1963b2c389358e71d265af3d95
tsteinme <tsteinme@hs-lausitz.de>
2012-12-08 16:58:02 +0100

    - RMatrixTest, RTableTest die Tests zu diesen Klassen verliefen erfolgreich.

---
+++ R_Interface/tests/de/hsl/rinterface/objects/RMatrixTest.java
@@ -0,0 +1,31 @@
+package de.hsl.rinterface.objects;
+
+import java.io.IOException;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import de.hsl.rinterface.exception.RException;
+
+public class RMatrixTest {
+
+	@Before
+	public void init() throws IOException, RException
+	{
+	}
+	
+	@Test
+	public void rtable() {
+		RMatrix m1 = new RMatrix(2, 3);
+		Assert.assertNotNull(m1);
+		Assert.assertEquals(3, m1.getColLength());
+		Assert.assertEquals(2, m1.getRowLength());
+		m1.setMatrix(new String[][] { { "1", "2","3" }, { "4", "5","6" } });
+		Assert.assertEquals(m1.getMatrixAt(1, 1), "5");
+	}
+	
+	@After
+	public void cleanUp() throws IOException, InterruptedException, RException
+	{	
+	}
+}
--- R_Interface/tests/de/hsl/rinterface/objects/RTableTest.java
+++ R_Interface/tests/de/hsl/rinterface/objects/RTableTest.java
@@ -1,12 +1,35 @@
 package de.hsl.rinterface.objects;
 
-public class RTableTest {
+import java.io.IOException;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import de.hsl.rinterface.exception.RException;
 
-	public static void main(String[] args) {
-		RTable t1 = new RTable(2, 2);
-		t1.setColTitle(new String[] { "a", "b" });
+public class RTableTest {
+	
+	@Before
+	public void init() throws IOException, RException
+	{
+	}
+	
+	@Test
+	public void rtable() {
+		RTable t1 = new RTable(2, 3);
+		Assert.assertNotNull(t1);
+		Assert.assertEquals(3, t1.getColLength());
+		Assert.assertEquals(2, t1.getRowLength());
+		t1.setColTitle(new String[] { "a", "b","c" });
+		Assert.assertEquals(t1.getColTitleAt(0), "a");
 		t1.setRowTitle(new String[] { "c", "d" });
-		t1.setMatrix(new String[][] { { "1", "2" }, { "3", "4" } });
-		System.out.println(t1.toString());
+		Assert.assertEquals(t1.getRowTitleAt(1), "d");
+		t1.setMatrix(new String[][] { { "1", "2","3" }, { "4", "5","6" } });
+		Assert.assertEquals(t1.getMatrixAt(1, 1), "5");
+	}
+	
+	@After
+	public void cleanUp() throws IOException, InterruptedException, RException
+	{	
 	}
 }
