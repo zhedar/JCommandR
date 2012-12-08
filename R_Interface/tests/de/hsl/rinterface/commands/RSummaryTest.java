d967b4c895ed93e6d1d2889ada694a5b4a0e18ad
tsteinme <tsteinme@hs-lausitz.de>
2012-12-08 17:39:01 +0100

    - RSummary erfolgreich getestet

---
+++ R_Interface/tests/de/hsl/rinterface/commands/RSummaryTest.java
@@ -0,0 +1,66 @@
+package de.hsl.rinterface.commands;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.hsl.rinterface.Connection;
+import de.hsl.rinterface.ConsoleConnection;
+import de.hsl.rinterface.exception.RException;
+import de.hsl.rinterface.objects.RMatrix;
+import de.hsl.rinterface.objects.RObject;
+import de.hsl.rinterface.objects.RObjectTypes;
+import de.hsl.rinterface.objects.RTable;
+import de.hsl.rinterface.objects.RValue;
+import de.hsl.rinterface.objects.RVector;
+
+public class RSummaryTest {
+
+	//Testverbindung
+	private Connection con;
+	private RVector<Double> werte;
+
+	@Before
+	public void init() throws IOException, RException
+	{
+		con = new ConsoleConnection();
+		werte = new RVector<>();
+		for (int i = 1; i <= 5; i++) {
+			werte.add((double) i);
+		}
+	}
+	
+	//Dies ist ein Beispiel
+		@Test
+		public void rmatrixtest() throws RException
+		{
+			RSummary rs = new RSummary(werte);
+			Assert.assertNotNull(rs);
+			Assert.assertEquals("summary(c(1.0, 2.0, 3.0, 4.0, 5.0))", rs.prepareForSending().trim());
+			RObject ro = con.sendCmd(rs.prepareForSending());
+			Assert.assertNotNull(ro);
+			Assert.assertEquals( RObjectTypes.TABLE, ro.getType());
+			RTable rt = (RTable)ro;
+			Assert.assertEquals(1, rt.getColLength());
+			Assert.assertEquals(6, rt.getRowLength());
+			Assert.assertEquals("Median", rt.getColTitleAt(2));
+			Assert.assertEquals("3", rt.getMatrixAt(0, 2));
+			Assert.assertEquals("1", rt.getMatrixAt(0, 0));
+			Assert.assertEquals("5", rt.getMatrixAt(0, 5));	
+		}
+	
+		
+	@After
+	public void cleanUp() throws IOException, InterruptedException, RException
+	{	//Connection schließen, wenn nicht bereits geschehen
+		if (con != null && con.isAlive())
+		{
+			con.close();
+			con = null;
+		}
+	}
+}
